8346bc308859c51ad37378a33f3362cb7dc94d9f
Danillo <danillo.damian@hotmail.com>
2022-08-26 12:05:10 -0300

    Added option to pause the game. Added instructions.

--- sketch.js
+++ sketch.js
@@ -1,4 +1,6 @@
 
+let pauseGame= false;
+
 function setup() {
   createCanvas(500, 400);
   backgroundSound.loop();
@@ -8,17 +10,32 @@ function draw() {
   background(roadImage);
   showPlayer();
   showCar();
-  moveCar();
-  movePlayer();
+  if(pauseGame==false){
+    moveCar();
+  movePlayer();}
   returnCarStart();
   checkCollision();
   score();
   increaseScore();
-  
+  keyPressed();
+  instructions()
 }
 
+function keyPressed() {
+  if (keyCode == 32) {
+    pauseGame = true;
+  } else if (keyCode == 13) {
+    pauseGame = false;
+  }
+}
 
-
+function instructions(){
+  textSize(15);
+  fill(color(255,255,100))
+   text("Use arrow keys to move",110,385 )
+  textSize(12);
+  text("Press space to pause and enter to return",380,385 )
+}
 
 
 
